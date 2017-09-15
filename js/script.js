b746ffc06d9a45a4f2c7b4d1cf9e1c9d1c1af68d
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-15 11:49:15 -0400

    feat(layout): add basic template

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var decodedData = window.atob("http://api.domain.com/glpi/apirest.php;45erjbudklq5865sdkjhjks;lkhjfkgsdf546634s;company's helpdesk;+33123456789;https://support.company.com;support@company.com;"); // decode the string
+
+console.log(decodedData)
