7b66457cc727cefe245245cc6713596e65312348
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-15 16:20:30 -0400

    feat(OS): add detection of OS

--- js/script.js
+++ js/script.js
@@ -3,6 +3,23 @@
 // var decodedData = window.atob(encodedData); // decode the string
 // console.log(decodedData)
 
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
+}
+
+
+console.log(getMobileOperatingSystem())
+
 var url_string = window.location.href
 var url = new URL(url_string)
 var data = url.searchParams.get("data")
