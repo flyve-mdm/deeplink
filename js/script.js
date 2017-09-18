574d8de345b8b1e047fec08bc0720ec4cf4dc526
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-18 14:41:15 -0400

    refactor(deeplinks): add corrections
    - change decoding of the URL
    - automatically hide the button if it is not on a mobile device
    - change the message of the button
    - increase the size of the QR
    - change design so that it resembles more to the mini sites
    - correct validation of ios by Gianfranco Manganiello

--- js/script.js
+++ js/script.js
@@ -3,79 +3,69 @@ function getMobileOperatingSystem() {
   var userAgent = navigator.userAgent || navigator.vendor || window.opera
 
     if (/android/i.test(userAgent)) {
-        return "Android";
+        return "Android"
     }
 
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return "iOS";
+        return "iOS"
     }
 
-    return "unknown";
+    return "unknown"
 }
+
+var OS = getMobileOperatingSystem()
 
 // get data of the url
 var url_string = window.location.href
 var url = new URL(url_string)
 var data = url.searchParams.get("data")
-var re = new RegExp(';')
-encodedData = data.split(re)
-
-function getDecodedData(item, index) {
-    return window.atob(item)
-}
-
-var decodedData = encodedData.map(getDecodedData);
+data = window.atob(data)
+var myRe = /\\;/g
+var decodedData = data.split(myRe)
 
 // QR
 var qr = new QRious({
             element: document.getElementById('qr'),
-            value: url_string
+            size: 200,
+            value: data
         })
 
 // name company
-document.getElementById('company_name').innerHTML = decodedData[0]
+document.getElementById('company_name').innerHTML = decodedData[3]
 
 // phone
 var phone = document.createElement("a")
-phone.setAttribute('href',`callto:${decodedData[1]}`)
-phone.textContent = decodedData[1]
+phone.setAttribute('href',`callto:${decodedData[4]}`)
+phone.textContent = decodedData[4]
 document.getElementById('phone').appendChild(phone)
 
 // url
 var url = document.createElement("a")
-url.setAttribute('href',decodedData[2])
-url.textContent = decodedData[2]
+url.setAttribute('href',decodedData[5])
+url.textContent = decodedData[5]
 document.getElementById('url').appendChild(url)
 
 // email
 var email = document.createElement("a")
-email.setAttribute('href',`mailto:${decodedData[3]}`)
-email.textContent = decodedData[3]
+email.setAttribute('href',`mailto:${decodedData[6]}`)
+email.textContent = decodedData[6]
 document.getElementById('email').appendChild(email)
 
-// button "Go to Flyve MDM"
-document.getElementById('btn').addEventListener('click', function() {
-    location.reload()
-})
-
-// redirect to the store in case of not opening the application
-var openFlyve = function () { 
-    var open = confirm("Open flyve MDM?")
-    if (open) {
+// button "Enroll Device"
+if ( OS == 'Android' || OS == 'iOS' ) {
+    var btn = document.createElement("button")
+    btn.textContent = "Enroll Device"
+    btn.addEventListener('click', function() {
+        window.location.replace(data)
         setTimeout( function()  { 
             if (!document.hidden) {
-                var OS = getMobileOperatingSystem()
                 if ( OS == 'Android') {
                     window.location = "https://play.google.com/store/apps/details?id=org.flyve.mdm.agent";
-                } else if (OS == iOS) {
+                } else if (OS == 'iOS') {
                     window.location = "https://itunes.apple.com/us/app/flyve-mdm-agent";
                 }
             }
-        }, 2000)
-    }
-}
-window.onload = openFlyve()
-
-
-
-
+        }, 3000)
+    })
+    document.getElementById('email').appendChild(btn)
+} 
