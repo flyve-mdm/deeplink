d7660996ea10179fcf54f128a6592a7eebce8fa1
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-06 14:43:02 -0400

    build(deeplink): update site

---
+++ js/script.js
@@ -0,0 +1,92 @@
+// detect OS
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera
+
+    if (/android/i.test(userAgent)) {
+        return "Android"
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS"
+    }
+
+    return "unknown"
+}
+
+var OS = getMobileOperatingSystem()
+
+// get data of the url
+var data = getData("data")
+
+var deeplink = `flyve://register?data=${data}`
+console.log(deeplink)
+data = window.atob(data)
+var myRe = /\\;/g
+var decodedData = data.split(myRe)
+
+// QR
+var qr = new QRious({
+            element: document.getElementById('qr'),
+            size: 200,
+            value: deeplink
+        })
+
+// name company
+document.getElementById('company_name').innerHTML = decodedData[3]
+
+// phone
+var phone = document.createElement("a")
+phone.setAttribute('href',`callto:${decodedData[4]}`)
+phone.textContent = decodedData[4]
+document.getElementById('phone').appendChild(phone)
+
+// url
+var url = document.createElement("a")
+url.setAttribute('href',decodedData[5])
+url.textContent = decodedData[5]
+document.getElementById('url').appendChild(url)
+
+// email
+var email = document.createElement("a")
+email.setAttribute('href',`mailto:${decodedData[6]}`)
+email.textContent = decodedData[6]
+document.getElementById('email').appendChild(email)
+
+var clickEvent = ((document.ontouchstart!==null)?'click':'touchstart')
+
+// button "Enroll Device"
+if ( OS == 'Android' || OS == 'iOS' ) {
+    var btn = document.createElement("a")
+    btn.textContent = "Enroll Device"
+    btn.setAttribute('href',deeplink)
+    btn.setAttribute('class','button')
+    btn.addEventListener(clickEvent, function() {
+        var timer = setTimeout( function()  { 
+            if (!document.hidden) {
+                if ( OS == 'Android') window.location.replace("market://details?id=org.flyve.mdm.agent")
+                else if (OS == 'iOS') window.location.replace("https://itunes.apple.com/us/app/flyve-mdm-agent")
+            } 
+        }, 4000)
+        document.addEventListener("visibilitychange", function() {
+            clearInterval(timer)
+        })
+    })
+    document.getElementById('btn').appendChild(btn)
+} 
+
+// button show
+if ( OS == 'Android' || OS == 'iOS' ) { 
+    document.getElementById('qr').className = "hidden" 
+    var btn_show = document.createElement("button")
+    btn_show.textContent = "Show QR to enroll another device"
+    btn_show.setAttribute('class','button')
+    btn_show.addEventListener(clickEvent, function() {
+        document.getElementById('qr').className = ""
+        document.getElementById('img').className = "hidden"
+        document.getElementById('btn-show').className = "hidden"
+    })
+    document.getElementById('btn-show').appendChild(btn_show)    
+}
+
+
+console.log(deeplink)
