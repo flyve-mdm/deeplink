643503bfcac14df185d4f18e826245d9d02c4796
Flyve MDM <ml-flyvemdm@teclib.com>
2018-09-03 15:13:35 +0000

    build(deeplink): update site

---
+++ config.example.js
@@ -0,0 +1,7 @@
+window.appConfig = {
+    defaultLocation: "http://example.org/",
+    bugsnagApiKey: 12345678,
+    deeplinkBase: "yourApp://customRoute",
+    androidDefaultLink: "the route of your application in the marketplace",
+    iosDefaultLink: "the route of your application in the applestotre"
+}
