f8e3ed3957f8e0b756e3727e2759f8e1545a6cc9
Flyve MDM <ml-flyvemdm@teclib.com>
2018-09-07 16:03:58 +0000

    build(deeplink): update site

---
+++ config.js
@@ -0,0 +1,7 @@
+window.appConfig = {
+    defaultLocation: "http://flyve.org/",
+    bugsnagApiKey: fab38c004e4e4cf378f09b64bf2929f6,
+    deeplinkBase: "flyve://register",
+    androidDefaultLink: "market://details?id=org.flyve.mdm.agent",
+    iosDefaultLink: "https://itunes.apple.com/us/app/flyve-mdm-agent"
+}
