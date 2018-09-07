0f29698a4a213ff1a9862b31954969233868e3c6
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-09-07 14:22:28 -0400

    fix(config): pass the api key of bugsnag to a string
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- site/config.example.js
+++ site/config.example.js
@@ -1,6 +1,6 @@
 window.appConfig = {
     defaultLocation: "http://example.org/",
-    bugsnagApiKey: 12345678,
+    bugsnagApiKey: "12345678",
     deeplinkBase: "yourApp://customRoute",
     androidDefaultLink: "the route of your application in the marketplace",
     iosDefaultLink: "the route of your application in the applestotre"
